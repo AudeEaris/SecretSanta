ae200a6d549ea54e167d2433cba5f31b5f9d833e
AudeEaris <aude.casteigts@gmail.com>
2024-03-13 16:47:17 +0100

    Created the unit tests for the secret santa function

---
+++ secret_santa.py
@@ -0,0 +1,13 @@
+class SecretSanta:
+    def __init__(self, friends, couples):
+        self.friends = friends
+        self.couples = couples
+
+
+    def controlled_allocation(self):
+        """
+        A function working with only one iteration returning a list of partners for the secret santa
+        :return:
+        secret_santa: list of tuples containing the partners for the gifts
+        """
+        return []
